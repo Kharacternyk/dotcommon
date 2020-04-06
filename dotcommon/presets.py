cdc31fb7003f133388a1ef6b9a19cc2aabc7443c
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-04-06 23:28:58 +0300

    Add vim_maps preset.

--- dotcommon/presets.py
+++ dotcommon/presets.py
@@ -1,4 +1,4 @@
-from dotcommon.util import lines_starting_with
+from dotcommon.util import lines_starting_with, preset, strip
 
 bash_paths = (".bashrc", "bashrc")
 bash_readline_macros = lines_starting_with("bind", "#"), bash_paths
@@ -6,3 +6,12 @@ bash_aliases = lines_starting_with("alias", "#"), bash_paths
 
 vim_paths = (".vimrc", "vimrc", ".vim/vimrc")
 vim_set_statements = lines_starting_with("set", '"'), vim_paths
+
+@preset(vim_paths)
+def vim_maps(text):
+    lines = text.splitlines()
+    for line in lines:
+        stripped = strip(line, '"')
+        space_index = line.find(" ")
+        if space_index != -1 and stripped[:space_index].endswith("map"):
+            yield stripped
