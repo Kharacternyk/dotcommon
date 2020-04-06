a47b85a3d3940d95c5f71cc860ca8fbdc21f3e2c
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-04-06 12:28:20 +0300

    Modularize the code a bit.

--- dotcommon.py
+++ dotcommon.py
@@ -6,26 +6,32 @@ from getpass import getpass
 from collections import Counter
 from pprint import pprint
 
-username = input("Username: ")
-password = getpass("Password: ")
-github = Github(username, password)
+def auth():
+    username = input("Username: ")
+    password = getpass("Password: ")
+    return Github(username, password)
 
-counter = Counter()
-dot_repos = github.search_repositories(query="topic:dotfiles")
-
-def strip_comment(line):
-    i = line.find('"')
+def strip_comment(line, comment_chars):
+    i = line.find(comment_chars)
     if i == -1:
         return line
     return line[:i]
 
-for i, repo in enumerate(dot_repos[:300]):
-    try:
-        vimrc = repo.get_contents(".vimrc")
-        lines = vimrc.decoded_content.decode("utf-8").splitlines()
-        counter.update(strip_comment(line) for line in lines)
-        print(i, "+1")
-    except Exception:
-        print(i, ":(")
+def count_lines(comment_chars, count, *paths):
+    github = auth()
+    counter = Counter()
 
-pprint(counter)
+    dot_repos = github.search_repositories(query="topic:dotfiles")
+
+    for i, repo in enumerate(dot_repos[:count]):
+        for path in paths:
+            try:
+                cfg = repo.get_contents(path)
+                lines = cfg.decoded_content.decode("utf-8").splitlines()
+                counter.update(strip_comment(line, comment_chars) for line in lines)
+                print(i)
+                break
+            except Exception:
+                pass
+
+    pprint(counter)
