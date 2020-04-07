f52122a4f796071e89c4721dd42e922b614b6ee9
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-04-07 22:41:04 +0300

    Use classes to group presets.

--- dotcommon/presets.py
+++ dotcommon/presets.py
@@ -1,11 +1,21 @@
 from dotcommon.util import lines_starting_with
 
-bash_paths = (".bashrc", "bashrc")
-bash_readline_macros = lines_starting_with("bind", "#")
-bash_aliases = lines_starting_with("alias", "#")
-bash_exports = lines_starting_with("export", "#")
 
-vim_paths = (".vimrc", "vimrc", ".vim/vimrc", "vim/.vimrc", "vim/vimrc")
-vim_set_statements = lines_starting_with("set", '"')
-vim_vundle = lines_starting_with("Plugin", '"')
-vim_plug = lines_starting_with("Plug ", '"')
+class Bash:
+    paths = (".bashrc", "bashrc")
+
+    readline_macros = lines_starting_with("bind", "#")
+    aliases = lines_starting_with("alias", "#")
+    exports = lines_starting_with("export", "#")
+
+    atomizers = readline_macros, aliases, exports
+
+
+class Vim:
+    paths = (".vimrc", "vimrc", ".vim/vimrc", "vim/.vimrc", "vim/vimrc")
+
+    set_statements = lines_starting_with("set", '"')
+    vundle = lines_starting_with("Plugin", '"')
+    plug = lines_starting_with("Plug ", '"')
+
+    atomizers = set_statements, vundle, plug
