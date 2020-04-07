217f76c83f3cd19da636d56a07d4620c0f87067b
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-04-07 20:22:05 +0300

    Improve vim_map_statements preset.

--- dotcommon/presets.py
+++ dotcommon/presets.py
@@ -5,7 +5,6 @@ bash_readline_macros = lines_starting_with("bind", "#"), bash_paths
 bash_aliases = lines_starting_with("alias", "#"), bash_paths
 
 vim_paths = (".vimrc", "vimrc", ".vim/vimrc", "vim/.vimrc", "vim/vimrc")
-vim_set_statements = lines_starting_with("set", '"'), vim_paths
 
 
 @preset(vim_paths)
@@ -13,10 +12,10 @@ def vim_map_statements(text):
     lines = text.splitlines()
     for line in lines:
         stripped = strip(line, '"')
-        space_index = line.find(" ")
-        if space_index != -1 and stripped[:space_index].endswith("map"):
+        words = stripped.split()
+        if len(words) > 2 and words[0].endswith("map"):
             yield stripped
 
-
+vim_set_statements = lines_starting_with("set", '"'), vim_paths
 vim_vundle = lines_starting_with("Plugin", '"'), vim_paths
 vim_plug = lines_starting_with("Plug ", '"'), vim_paths
