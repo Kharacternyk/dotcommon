baaebdbb4a235ec9f5cc7112a0038c7265e7082c
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-04-07 13:26:50 +0300

    Add more vim paths.

--- dotcommon/presets.py
+++ dotcommon/presets.py
@@ -4,7 +4,7 @@ bash_paths = (".bashrc", "bashrc")
 bash_readline_macros = lines_starting_with("bind", "#"), bash_paths
 bash_aliases = lines_starting_with("alias", "#"), bash_paths
 
-vim_paths = (".vimrc", "vimrc", ".vim/vimrc")
+vim_paths = (".vimrc", "vimrc", ".vim/vimrc", "vim/.vimrc", "vim/vimrc")
 vim_set_statements = lines_starting_with("set", '"'), vim_paths
 
 
