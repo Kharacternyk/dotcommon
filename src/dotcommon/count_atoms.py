53a5da3028903d249f5c5e4e83dcadaff7ee97b1
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-04-09 20:21:15 +0300

    Catch IndexError in count_atoms.

--- src/dotcommon/count_atoms.py
+++ src/dotcommon/count_atoms.py
@@ -9,10 +9,14 @@ def count_atoms(files, atomizers):
     for file in files:
         try:
             text = file.decoded_content.decode("utf-8")
-            for counter, atomizer in counters_atomizers:
-                counter.update(atomizer(text))
         except (GithubException, AssertionError, UnicodeDecodeError, TypeError) as e:
             print(e, file=stderr)
             pass
+        except IndexError:
+            print(f"Only {len(files)} have been found", file=stderr)
+            break
+        else:
+            for counter, atomizer in counters_atomizers:
+                counter.update(atomizer(text))
 
     return counters_atomizers
