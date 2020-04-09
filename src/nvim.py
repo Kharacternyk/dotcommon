3a57a4c3a592c38d79249417137d5a27c40302de
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-04-09 23:17:27 +0300

    Add Neovim preset.

---
+++ src/nvim.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from dotcommon.util import lines_starting_with
+from dotcommon.crawl import crawl
+
+
+def set_statements(text):
+    "Set statements"
+    return lines_starting_with("set ", text)
+
+
+def vundle(text):
+    "Vundle plugins"
+    return lines_starting_with("Plugin ", text)
+
+
+def plug(text):
+    "Vim-plug plugins"
+    return lines_starting_with("Plug ", text)
+
+
+def custom_functions(text):
+    "Custom functions per vimrc"
+    count = 0
+    for line in text.splitlines():
+        stripped = line.rstrip()
+        if stripped.startswith("endfun"):
+            count += 1
+    # A tuple!
+    return (count,)
+
+
+crawl("Neovim", "init.vim", set_statements, vundle, plug, custom_functions)
