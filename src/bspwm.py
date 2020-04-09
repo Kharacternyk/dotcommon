2b590041453dd9cafc1bba0ff7d4b902baa6e326
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-04-09 15:57:28 +0300

    Add bspwm preset.

---
+++ src/bspwm.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from dotcommon.util import lines_starting_with, strip
+from dotcommon.crawl import crawl
+
+
+@lines_starting_with("Window rules", "bspc rule ", "#")
+def window_rules():
+    pass
+
+
+def gap_border(text):
+    "Window gap/border width"
+    gap = "unset"
+    border = "unset"
+    for line in text.splitlines():
+        stripped = strip(line, "#")
+        if stripped.startswith("bspc config window_gap "):
+            gap = stripped[: stripped.rfind(" ") : -1]
+        elif stripped.startswith("bspc config border_width "):
+            border = stripped[: stripped.rfind(" ") : -1]
+    # Tuple!
+    return (f"{gap}/{border}",)
+
+
+crawl("bspwm", "bspwmrc", window_rules, gap_border)
