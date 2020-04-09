5d910cd74d614dc13244f9f09d5183cb1de0e47d
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-04-09 21:08:50 +0300

    sxhkd: add a separate chart of non-bspc commands.

--- src/sxhkd.py
+++ src/sxhkd.py
@@ -7,8 +7,13 @@ from dotcommon.crawl import crawl
 def commands(text):
     "Commands bound to keybinds"
     indented = lines_starting_with(" ", text)
-    stripped = (line.strip() for line in indented)
+    stripped = (line.strip() for line in indented if not line.isspace())
     return stripped
 
 
-crawl("sxhkd", "sxhkdrc", commands)
+def commands_no_bspc(text):
+    "Commands bound to keybinds (except bspc)"
+    return (command for command in commands(text) if command.split()[0] != "bspc")
+
+
+crawl("sxhkd", "sxhkdrc", commands, commands_no_bspc)
