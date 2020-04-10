e8652dcdbbb88415795232eaab7145067a3cb2d3
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-04-10 13:22:24 +0300

    Do not catch IndexError, it is unrecoverable.

--- src/dotcommon/count_atoms.py
+++ src/dotcommon/count_atoms.py
@@ -6,17 +6,19 @@ from sys import stderr
 def count_atoms(files, atomizers):
     counters_atomizers = [(Counter(), atomizer) for atomizer in atomizers]
 
-    try:
-        for file in files:
-            try:
-                text = file.decoded_content.decode("utf-8")
-            except (GithubException, AssertionError, UnicodeDecodeError, TypeError) as e:
-                print(e, file=stderr)
-                pass
-            else:
-                for counter, atomizer in counters_atomizers:
-                    counter.update(atomizer(text))
-    except IndexError:
-        print("Interrupted")
+    for file in files:
+        try:
+            text = file.decoded_content.decode("utf-8")
+        except (
+            GithubException,
+            AssertionError,
+            UnicodeDecodeError,
+            TypeError,
+        ) as e:
+            print(e, file=stderr)
+            pass
+        else:
+            for counter, atomizer in counters_atomizers:
+                counter.update(atomizer(text))
 
     return counters_atomizers
