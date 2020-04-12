982e89c97e1292d7a901ecc1885b6dca2ba7200f
Nazar Vinnichuk <nazar.vinnichuk@tutanota.com>
2020-04-12 12:01:17 +0300

    Reformat zsh.py via black.

--- src/zsh.py
+++ src/zsh.py
@@ -8,16 +8,20 @@ def aliases(text):
     "Aliases"
     return lines_starting_with("alias ", text)
 
+
 def theme(text):
     "Theme"
     return lines_starting_with("ZSH_THEME=", text)
+
 
 def exports(text):
     "Exports"
     return lines_starting_with("export ", text)
 
+
 def keybindings(text):
     "Keybindings"
     return lines_starting_with("bindkey ", text)
 
-crawl("ZSH", "zshrc", aliases, exports, theme, keybindings)
+
+crawl("Zsh", "zshrc", aliases, exports, theme, keybindings)
